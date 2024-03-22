80e00b3d3af7389b2e7dc64f1556ab23dcd8c0a4
Rootdiv <rootdiv9152@gmail.com>
2024-03-22 14:14:44 +0300

    feat: changed api server

--- src/redux/pizza/asyncActions.ts
+++ src/redux/pizza/asyncActions.ts
@@ -2,18 +2,21 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { Pizzas, SearchPizzaParams } from './types';
 
-const URL = `${window.location.protocol}//${window.location.hostname}:2010`;
+const URL = import.meta.env.DEV ? 'http://localhost:2010' : 'https://api.rootdiv.ru';
 
 //createAsyncThunk<Pizzas, SearchPizzaParams> - типизация ответа и аргументов функции.
-export const fetchPizzas = createAsyncThunk<Pizzas, SearchPizzaParams>('pizza/fetchPizzasStatus', async params => {
-  const { sortBy, categoryId, currentPage, search } = params;
+export const fetchPizzas = createAsyncThunk<Pizzas, SearchPizzaParams>(
+  'pizza/fetchPizzasStatus',
+  async params => {
+    const { sortBy, categoryId, currentPage, search } = params;
 
-  const categorySort = categoryId !== 0 ? `category=${categoryId}&` : '';
-  const sortType = sortBy.type.replace('-', '');
-  //axios.get<Pizzas> - типизация ответа.
-  const { data } = await axios.get<Pizzas>(
-    `${URL}/pizzas?page=${currentPage}&limit=4&${categorySort}sortby=${sortType}&order=${sortBy.order}${search}`,
-  );
+    const categorySort = categoryId !== 0 ? `category=${categoryId}&` : '';
+    const sortType = sortBy.type.replace('-', '');
+    //axios.get<Pizzas> - типизация ответа.
+    const { data } = await axios.get<Pizzas>(
+      `${URL}/pizzas?page=${currentPage}&limit=4&${categorySort}sortby=${sortType}&order=${sortBy.order}${search}`,
+    );
 
-  return data;
-});
+    return data;
+  },
+);
